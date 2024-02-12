a0363cfcc275c0d8d06dee9fe3a788e9bf0a457f
Diego <diego.o.guimaraes29@gmail.com>
2024-02-12 15:57:36 -0300

    feat: create class LojaNetDal and your interface

---
+++ LojaNet/LojaNet.DAL/ClienteDal.cs
@@ -0,0 +1,42 @@
+﻿using LojaNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.DAL
+{
+    public class ClienteDal : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
