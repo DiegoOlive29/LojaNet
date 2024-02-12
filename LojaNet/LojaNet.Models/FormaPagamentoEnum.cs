e2c1b47d5207e8c33ad4130993fa114a619c8fc7
Diego <diego.o.guimaraes29@gmail.com>
2024-02-12 13:41:16 -0300

    feat : create class base

---
+++ LojaNet/LojaNet.Models/FormaPagamentoEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.Models
+{
+    public enum FormaPagamentoEnum
+    {
+        Dinheiro = 1,
+
+        Cartão =2,
+
+        Boleto =3,
+
+        Deposito = 4,
+
+
+
+    }
+}
