db35f825ee2476c399575ff6ff02db5ae6de4313
Diego <diego.o.guimaraes29@gmail.com>
2024-02-15 16:42:01 -0300

    feat : crate path post

--- LojaNet/LojaNet.Ui.Web/Controllers/ClienteController.cs
+++ LojaNet/LojaNet.Ui.Web/Controllers/ClienteController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LojaNet.Models;
+using LojaNet.BLL;
+
 namespace LojaNet.Ui.Web.Controllers
 {
     public class ClienteController : Controller
@@ -14,6 +16,26 @@ namespace LojaNet.Ui.Web.Controllers
             return View(cli);
 
         }
+        [HttpPost]
+
+        public ActionResult Incluir(Cliente cliente)
+        {
+            try { 
+            var bll = new ClienteBLL();
+            bll.Incluir(cliente);
+
+            return RedirectToAction("Index");
+            }catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+
+                return View(cliente);
+
+            }
+
+
+        }
+
         public IActionResult Index()
         {
             return View();
