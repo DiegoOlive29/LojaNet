530b90852f34c8de950f5d748cbd907c320da16f
Diego <diego.o.guimaraes29@gmail.com>
2024-02-15 13:59:29 -0300

    feat: create form post

--- LojaNet/LojaNet.Ui.Web/Controllers/ClienteController.cs
+++ LojaNet/LojaNet.Ui.Web/Controllers/ClienteController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using LojaNet.Models;
 namespace LojaNet.Ui.Web.Controllers
 {
     public class ClienteController : Controller
     {
+
+        public ActionResult Incluir()
+        {
+            var cli = new Cliente();
+
+
+
+            return View(cli);
+
+        }
         public IActionResult Index()
         {
             return View();
